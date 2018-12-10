c61a3ae293d05f75916b604894441d137298b369
unknown <noynir100@gmail.com>
2018-12-10 19:23:58 +0200

    post hackaton

---
+++ Code/U.py
@@ -0,0 +1,56 @@
+from math import e
+
+import sys
+
+
+def bisection(a, b, f, epsilon):
+    """
+    :param a: first value to check in the function
+    :param b: second value to check in the function
+    :param f: The function to check
+    :param epsilon: error tolerance
+    :return: m: the root of the function
+    """
+    m = (a + b) / 2.0
+    while abs(a - b) > epsilon:
+        if f(m) == 0:
+            return m
+        elif f(a) * f(m) < 0:
+            b = m
+        else:
+            a = m
+        m = (a + b) / 2.0
+    return m
+
+
+def newton_rapson(func, x=0, it=1, h=0.00000001):
+    """
+    :param func: the function to get it's roots
+    :param x: starting guess(default=0)
+    :param it: the maximum number of allowed iterations(default=1)
+    :param h: accuracy factor of the method (default=0.00000001)
+    :return: An estimated root of the funtion
+    """
+
+    def der(f, h):
+        return lambda x0: (f(x0 + h) - f(x0)) / h
+
+    if der(func, h)(x) == 0:
+        x = 1
+    for i in range(it):
+        fd = func(x)
+        ft = der(func, h)(x)
+        x = x - (fd / ft)
+    return x
+
+
+def func(x):
+    return x * pow(e, -1 * x) - 0.25
+
+
+y = newton_rapson(func, 3, it=100)
+b = bisection(-1 * y, y, func, 0.0000001)
+while b > y:
+    y = b
+    b = bisection(-1 * y, y, func, 0.0000001)
+u = b / 100
